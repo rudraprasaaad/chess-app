52c947fa963c68989ba982ad1016ce24f81f21b1
rudraprasaaad <goudarudra23@gmail.com>
2025-07-14 22:10:04 +0530

    changed readme

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -60,7 +60,7 @@ class Application {
 
     this.app.use(
       cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5371",
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         credentials: true,
       })
     );
