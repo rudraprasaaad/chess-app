3419f09f68b8eb10d5e644928ff146bbb63bec8a
rudraprasaaad <goudarudra23@gmail.com>
2025-07-15 17:21:26 +0530

    feat: added types

---
+++ backend/src/lib/types.ts
@@ -0,0 +1,70 @@
+import WebSocket from "ws";
+import { JsonValue } from "@prisma/client/runtime/library";
+
+export enum AuthProvider {
+  GOOGLE = "GOOGLE",
+  GUEST = "GUEST",
+}
+
+export enum UserStatus {
+  OFFLINE = "OFFLINE",
+  ONLINE = "ONLINE",
+  WAITING = "WAITING",
+  IN_GAME = "IN_GAME",
+  DISCONNECTED = "DISCONNECTED",
+}
+
+export enum GameStatus {
+  ACTIVE = "ACTIVE",
+  COMPLETED = "COMPLETED",
+  ABANDONED = "ABANDONED",
+}
+
+export enum RoomStatus {
+  OPEN = "OPEN",
+  ACTIVE = "ACITVE",
+  CLOSED = "CLOSED",
+}
+
+export enum RoomType {
+  PUBLIC = "PUBLIC",
+  PRIVATE = "PRIVATE",
+}
+
+export interface Player {
+  userId: string;
+  color: string;
+}
+
+export interface Room {
+  id: string;
+  type: RoomType;
+  status: RoomStatus;
+  players: { id: string; color: string | null }[];
+  inviteCode?: string;
+  createdAt: Date;
+}
+
+export interface Game {
+  id: string;
+  roomId: string;
+  fen: string;
+  moveHistory: JsonValue[];
+  timers: { white: number; black: number };
+  status: GameStatus;
+  players: Player[];
+  chat: JsonValue[];
+  winnerId?: string;
+  createdAt: Date;
+}
+
+export interface WebSocketMessage {
+  type: string;
+  payload: any;
+}
+
+export interface AuthenticatedWebSocket extends WebSocket {
+  playerId: string;
+  gameId?: string;
+  roomId?: string;
+}
