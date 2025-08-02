c63b4340d685b0bd0a05efa700ace0195c7d1309
rudraprasaaad <goudarudra23@gmail.com>
2025-08-03 02:00:31 +0530

    removed toast

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { useAuthStore } from "../store/auth";
-import { toast } from "sonner";
 import { Navigate } from "react-router-dom";
 
 interface ProtectedRouteProps {
@@ -10,22 +9,9 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { isAuthenticated } = useAuthStore();
 
-  const toastShownRef = useRef(false);
+  if (!isAuthenticated) return <Navigate to="/" />;
 
-  useEffect(() => {
-    if (!isAuthenticated && !toastShownRef.current) {
-      toast.error("You need to be logged in to access this page.", {
-        duration: 3000,
-      });
-      toastShownRef.current = true;
-    }
-  }, [isAuthenticated]);
-
-  if (!isAuthenticated) {
-    return <Navigate to="/" />;
-  }
-
-  return <>{children}</>;
+  return children;
 };
 
 export default ProtectedRoute;
